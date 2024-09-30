1b03ec60eedd75715126f3307fa26aee30d45713
NuStar <3318029085@qq.com>
2024-09-30 23:57:22 +0800

    功能性更新 + 增加按钮变量解析方法 = 优化按钮ItemStack 不再重复生成对象

--- src/main/java/com/rihejiu/nustarlib/menu/MenuButton.java
+++ src/main/java/com/rihejiu/nustarlib/menu/MenuButton.java
@@ -1,10 +1,13 @@
 package com.rihejiu.nustarlib.menu;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuButton {
@@ -27,12 +30,19 @@ public class MenuButton {
     private final String name;
     private final String material;
     private final List<String> lore;
-    private int data;
+    private final int data;
+
+    public ItemStack getButton() {
+        return button;
+    }
+
+    private final ItemStack button;
     public MenuButton(ConfigurationSection section) {
         this.name = section.getString("name");
         this.material = section.getString("material");
         this.lore = section.getStringList("lore");
         this.data = section.getInt("data", 0);
+        button = crate();
     }
     public ItemStack crate() {
         ItemStack itemStack = new ItemStack(Material.getMaterial(this.material.toUpperCase()));
@@ -41,6 +51,16 @@ public class MenuButton {
         itemMeta.setLore(this.lore);
         itemStack.setItemMeta(itemMeta);
         itemStack.setDurability((short) data);
+        return itemStack;
+    }
+    public ItemStack getPapiItem(Player player) {
+        ItemStack itemStack = button.clone();
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (!itemMeta.hasLore()) return itemStack;
+        List<String> lore1 = new ArrayList<>(itemMeta.getLore());
+        lore1.replaceAll(text -> PlaceholderAPI.setPlaceholders(player, text));
+        itemMeta.setLore(lore1);
+        itemStack.setItemMeta(itemMeta);
         return itemStack;
     }
 }
